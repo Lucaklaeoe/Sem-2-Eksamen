b4089cdb3f75666cabf721c819860b5b15bc181f
Lucaklaeoe <lucaklaeoeskole@gmail.com>
2024-05-06 14:48:46 +0200

    nav bar videre

---
+++ Hjemmeside/script.js
@@ -0,0 +1,15 @@
+//navbar variabler
+let burgerbar = true;
+const navbar_icon = document.getElementById("navbar_icon");
+function chance_navbar(){
+    if(burgerbar){
+        //burgerbar lukket
+        navbar_icon.setAttribute("d","M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM64 256c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H96c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z");
+    }
+    else{
+        //burgerbar open
+        navbar_icon.setAttribute("d","M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z");
+    }
+    burgerbar = !burgerbar;
+}
+
